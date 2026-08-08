d308bfa8aef60794c429e2a23c8ce021d0dc8486
Vikrant Singh <vikrantsinghtomar011@gmail.com>
2026-08-08 19:49:16 +0530

    fix: ensure trailing slash on BASE_URL concatenation across all components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://diluteoxygen.github.io',
-  base: '/TCS-NQT-PYQ',
+  base: '/TCS-NQT-PYQ/',
   integrations: [sitemap()],
   trailingSlash: 'never',
 });
